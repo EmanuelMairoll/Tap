6f647c377429695f1a645f959e84cf840f257967
Emanuel Mairoll <emanuel.mairoll@authenticvision.com>
2023-03-26 12:15:38 +0200

    Create WorkTimeUtil formula

---
+++ worktimeutil.rb
@@ -0,0 +1,19 @@
+class Worktimeutil < Formula
+  desc "Small CLI tool to read working hours from my calendar, sum them up and push them to absence.io"
+  homepage "https://github.com/EmanuelMairoll/WorkTimeUtil"
+  head "https://github.com/EmanuelMairoll/WorkTimeUtil.git"
+  license "MIT"
+
+  depends_on xcode: ["10.0", :build]
+
+  def install
+    system "xcodebuild", "-project", "WorkTimeUtil.xcodeproj", "-scheme", "WorkTimeUtil", "-configuration", "Release", "SYMROOT=build"
+    bin.install "build/Release/WorkTimeUtil"
+    bin.install_symlink "WorkTimeUtil" => "wtc"
+    bin.install_symlink "WorkTimeUtil" => "wtp"
+  end
+
+  test do
+    system "false"
+  end
+end
