5b5bdf1ed4b4f36634b37674adf4685281e2a5e3
Emanuel Mairoll <emanuel.mairoll@authenticvision.com>
2023-04-13 22:11:02 +0200

    Bump WorkTimeUtil to v1.1.0

--- worktimeutil.rb
+++ worktimeutil.rb
@@ -1,7 +1,7 @@
 class Worktimeutil < Formula
   desc "Small CLI tool to read working hours from my calendar, sum them up and push them to absence.io"
   homepage "https://github.com/EmanuelMairoll/WorkTimeUtil"
-  url "https://github.com/EmanuelMairoll/WorkTimeUtil.git", tag: "v1.0.1"
+  url "https://github.com/EmanuelMairoll/WorkTimeUtil.git", tag: "v1.1.0"
   head "https://github.com/EmanuelMairoll/WorkTimeUtil.git"
   license "MIT"
 
